cb94068eda93524ec9100f0d5acaa63aaa2ae8b7
stevemac321 <stevemac321@live.com>
2024-08-07 07:59:19 -0700

    added standish queue, kinda like sedgewick

--- Stack/queue.hpp
+++ Stack/queue.hpp
@@ -1,19 +1,19 @@
 #pragma once
 #include <iostream>
 
-template <typename T> class queue {
-	T que[1000000];
+template <typename T, int N= 1000000> class CLRS_queue {
+	T que[N];
 	int head = 0;
 	int tail = 0;
-	int len = 1000000;
+	int len = N;
 
 
 public:
-	
-	void enqueue(const T& x) 
+
+	void enqueue(const T& x)
 	{
 		que[tail] = x;
-		if (tail == (len-1) || tail < 0) {
+		if (tail == (len - 1) || tail < 0) {
 			std::cout << "que is full";
 			return;
 		}
@@ -21,10 +21,10 @@ public:
 			tail++;
 		}
 	}
-	T dequeue() 
+	T dequeue()
 	{
 		auto x = que[head];
-		if (head == (len -1) || head < 0) {
+		if (head == (len - 1) || head < 0) {
 			std::cout << "que is full";
 			return T();
 		}
@@ -33,4 +33,47 @@ public:
 		}
 		return x;
 	}
+	
+};
+template <typename T, int N= 1000000> class queue {
+	T que[N];
+	int head = 0;
+	int tail = 0;
+	int count = 0;
+	int len = N;
+
+
+public:
+
+	void enqueue(const T& x)
+	{
+		
+		if (is_full()) {
+			std::cout << "que is full";
+			return;
+		}
+		else {
+			que[tail] = x;
+			tail = (tail + 1) % len;
+			count++;
+		}
+	}
+	T dequeue()
+	{
+		T x = T();
+		if (is_empty()) {
+			std::cout << "que is empty";
+			x;
+		}
+		else {
+			x = que[head];
+			head = (head + 1) % len;
+			count--;
+		}
+		return x;
+	}
+	bool is_empty() { return count == 0; }
+	bool is_full() { return count == 0; }
+
+
 };
